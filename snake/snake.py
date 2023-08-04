8868e75a9eac015e3e999810f80f7c61fa1ce24c
eleahburman <eleahjburman@gmail.com>
2023-08-04 10:53:35 -0400

    Create event handlers for key up, key down, key left, key right

--- snake/snake.py
+++ snake/snake.py
@@ -17,11 +17,32 @@ orange=(255,150,20)
 teal=(135,191,170)
 dis.fill(grey)
 game_over=False
+x=300
+y=200
+x_change=0
+y_change=0
 while(game_over==False):
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-    pygame.draw.rect(dis,teal,(580,380,10,10))
+            
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_UP:
+                x_change=0
+                y_change=-10
+            elif event.key==pygame.K_DOWN:
+                x_change=0
+                y_change=10
+            elif event.key==pygame.K_RIGHT:
+                x_change=10
+                y_change=0
+            elif event.key==pygame.K_LEFT:
+                x_change=-10
+                y_change=0
+                
+    x=x+x_change
+    y=y+y_change
+    pygame.draw.rect(dis,teal,(x,y,10,10))
     pygame.display.update()
 pygame.quit()

8fe482301b1dd7dbc461faa77b51cd3b03ec6da7
eleahburman <eleahjburman@gmail.com>
2023-08-04 10:19:27 -0400

    Add red square

--- snake/snake.py
+++ snake/snake.py
@@ -3,11 +3,16 @@ pygame.init()
 #make variable
 dis=pygame.display.set_mode((600,400))
 pygame.display.update()
+red=(128,0,0)
+black=(0,0,0)
+white=(255,255,255)
+pygame.display.set_caption("Snake by Eleah Burman")
 game_over=False
 while(game_over==False):
-    pygame.display.update()
-    pygame.display.set_caption("Snake by Eleah Burman")
+    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
+    pygame.draw.rect(dis,red,(300,200,10,10))
+    pygame.display.update()
 pygame.quit()

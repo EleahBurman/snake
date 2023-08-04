c9ce4da90093556ae54c8cfc1081e0b3329ff15a
eleahburman <eleahjburman@gmail.com>
2023-08-04 10:12:55 -0400

    Add caption

--- snake/snake.py
+++ snake/snake.py
@@ -6,6 +6,7 @@ pygame.display.update()
 game_over=False
 while(game_over==False):
     pygame.display.update()
+    pygame.display.set_caption("Snake by Eleah Burman")
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True

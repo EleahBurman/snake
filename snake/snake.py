4789f4a92a9097d9fe90442eaafd3b6012c0ff0d
eleahburman <eleahjburman@gmail.com>
2023-08-04 10:08:14 -0400

    Create loop for display and game over

--- snake/snake.py
+++ snake/snake.py
@@ -2,3 +2,11 @@ import pygame
 pygame.init()
 #make variable
 dis=pygame.display.set_mode((600,400))
+pygame.display.update()
+game_over=False
+while(game_over==False):
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over=True
+pygame.quit()

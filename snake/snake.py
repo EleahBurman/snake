6a6295e1977794a5f78b81e29e697e5097b15ff2
eleahburman <eleahjburman@gmail.com>
2023-08-04 10:35:40 -0400

    Position square and change color to teal

--- snake/snake.py
+++ snake/snake.py
@@ -1,18 +1,27 @@
+#importing library
 import pygame
+
+#initializing pygame
 pygame.init()
+
 #make variable
 dis=pygame.display.set_mode((600,400))
+pygame.display.set_caption("Snake by Eleah Burman")
 pygame.display.update()
+#initializing color
 red=(128,0,0)
 black=(0,0,0)
 white=(255,255,255)
-pygame.display.set_caption("Snake by Eleah Burman")
+grey=(150,150,150)
+orange=(255,150,20)
+teal=(135,191,170)
+dis.fill(grey)
 game_over=False
 while(game_over==False):
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-    pygame.draw.rect(dis,red,(300,200,10,10))
+    pygame.draw.rect(dis,teal,(580,380,10,10))
     pygame.display.update()
 pygame.quit()

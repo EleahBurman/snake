e6cf7e87823d0be523fcd297378193119eb50f2c
eleahburman <eleahjburman@gmail.com>
2023-08-04 11:17:38 -0400

    Add game over and quit functionality

--- snake/snake.py
+++ snake/snake.py
@@ -1,33 +1,39 @@
-#importing library
 import pygame
-
-#initializing pygame
 pygame.init()
-
 #make variable
 dis=pygame.display.set_mode((600,400))
 pygame.display.set_caption("Snake by Eleah Burman")
 pygame.display.update()
-#initializing color
 red=(128,0,0)
 black=(0,0,0)
 white=(255,255,255)
 grey=(150,150,150)
 orange=(255,150,20)
-teal=(135,191,170)
+teal=(125,191,170)
 dis.fill(grey)
 game_over=False
+game_close=False
 x=300
 y=200
 x_change=0
 y_change=0
 clock=pygame.time.Clock()
 while(game_over==False):
-    
+    while(game_close==True):
+        dis.fill(white)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                game_over=True
+                game_close=False
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_q:
+                    game_over=True
+                    game_close=False
+                    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-            
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_UP:
                 x_change=0
@@ -41,9 +47,12 @@ while(game_over==False):
             elif event.key==pygame.K_LEFT:
                 x_change=-10
                 y_change=0
-                
+    
+    if x>=600 or x<0 or y>=400 or y<0:
+        game_close=True
     x=x+x_change
     y=y+y_change
+    dis.fill(grey)
     pygame.draw.rect(dis,teal,(x,y,10,10))
     pygame.display.update()
     clock.tick(30)
